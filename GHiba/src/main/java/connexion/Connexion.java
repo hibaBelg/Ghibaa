1176fe668982bafb26388bc86b362ec717adde97
hibaBelg <84625519+hibaBelg@users.noreply.github.com>
2022-01-22 13:10:23 +0100

    Update Connexion.java

--- GHiba/src/main/java/connexion/Connexion.java
+++ GHiba/src/main/java/connexion/Connexion.java
@@ -6,34 +6,34 @@ import java.sql.SQLException;
 
 public class Connexion {
 
-    private static String login = "uqn5zqzmxasp4odv";
-    private static String password = "pkoeejia7b8vjv93";
+	private static String login = "root";
+	private static String password = "";
 //    private static String url = "jdbc:mysql://localhost/gmachine";
-    private static String url = "jdbc:mysql://uqn5zqzmxasp4odv:pkoeejia7b8vjv93@i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qufrpew79thl5tzy";
-    private Connection connection = null;
-    private static Connexion instane;
+	private static String url = "jdbc:mysql://i8obauya8ad8cdog:oe46qo01adcqujy7@kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ulgfqpmmijv720vi";
+	private Connection connection = null;
+	private static Connexion instane;
 
-    private Connexion() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, login, password);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver introvable");
-        } catch (SQLException e) {
-            System.out.println("Connexion errror");
-            System.out.println(e.getMessage());
-        }
-    }
+	private Connexion() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url, login, password);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver introvable");
+		} catch (SQLException e) {
+			System.out.println("Connexion errror");
+			System.out.println(e.getMessage());
+		}
+	}
 
-    public Connection getConnection() {
-        return connection;
-    }
+	public Connection getConnection() {
+		return connection;
+	}
 
-    public static Connexion getInstane() {
-        if (instane == null) {
-            instane = new Connexion();
-        }
-        return instane;
-    }
+	public static Connexion getInstane() {
+		if (instane == null) {
+			instane = new Connexion();
+		}
+		return instane;
+	}
 
 }
